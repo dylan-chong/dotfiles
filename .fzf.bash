75b689c0687e2f8ab0d80d1b6c495eb768260d3e
Dylan Chong <dylan.chong@icloud.com>
2018-06-20 16:10:04 +1200

    Get fuzzy finder branch list working

--- .fzf.bash
+++ .fzf.bash
@@ -13,20 +13,15 @@ fi
 source "/usr/local/opt/fzf/shell/key-bindings.bash"
 
 fbr() {
-  local branches tags all
-
   # list branches, including remotes
   # - remove star on current branch, and leading spaces
   # - remove remote/ from each line
   # - convert stuff like "origin/master" into "origin/master\nmaster"
-  branches=`git branch -a \
+  `git branch -a \
     | perl -pe 's/^\s*\*?\s*//' \
     | perl -pe 's/remotes\///' \
-    | perl -pe 's/(\w+)\/(.*)/\1\/\2\n\2/'`
-
-  tags=`git tag -l`
-
-  all="$branches $tags"
-  # TODO doesn't break branches into mulitple lines
-  echo $all | fzf
+    | perl -pe 's/(\w+)\/(.*)/\1\/\2\n\2/' \
+    | sort \
+    | uniq \
+    | fzf`
 }
