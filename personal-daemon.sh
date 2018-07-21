7d1c21caedc1b89585d2bbbeeb98fc0977576ea1
Dylan Chong <dylan.chong@icloud.com>
2018-07-21 22:40:58 +1200

    Added a personal damon for cpu limit

---
+++ personal-daemon.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+while true; do
+    echo 'Killing AppPolice'
+    killall AppPolice
+
+    sleep 1
+    echo 'Starting AppPolice'
+    open -a 'AppPolice'
+
+    echo 'Waiting for next cycle...'
+    sleep 60
+done
