2b0ffebdf7b423f0016265fd1a7a5768f45f919d
Dylan Chong <dylan.chong@icloud.com>
2024-01-21 12:25:57 +0000

    Use old vim-session plugin from old nvim setup

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -24,11 +24,30 @@ lvim.format_on_save.enabled = true
 --------------- Custom plugins ---------------
 
 lvim.plugins = {
-  -- TODO this doesn't save wintabs buffers
+  -- Using old session plugin because it works with wintabs.
+  -- In the future, will use scope.nvim and a session plugin that goes with tt
   {
-    'rmagatti/auto-session',
+    'dylan-chong/vim-session',
+    dependencies = {
+      'xolox/vim-misc'
+    },
     config = function()
-      require("auto-session").setup {}
+      vim.api.nvim_exec2([[
+        " Settings to get it to work like vim-obsession (save session in current directory)
+        let g:session_autosave_silent = 1
+        let g:session_autoload = 'no'
+        let g:session_autosave = 'yes'
+        let g:session_lock_enabled = 0
+        let g:session_directory = './'
+        let g:session_default_name = 'Session'
+        let g:session_autosave_periodic = 0
+        let g:session_autosave_only_with_explicit_session = 1
+        augroup vim_session_autosave
+          au!
+          au FocusLost,BufWritePost,VimLeave * silent! call xolox#session#auto_save()
+        augroup END
+        let g:session_persist_colors = 0
+      ]], {})
     end,
   },
 
@@ -171,6 +190,7 @@ function CopyPath(expand)
   vim.fn.setreg('+', string)
   print('Copied: ' .. string)
 end
+
 lvim.builtin.which_key.mappings['%'] = {
   ":lua CopyPath('%')<Left><Left><C-f>i",
   "Copy file path"
