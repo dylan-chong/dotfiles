d8a89146ac7ee9c0d7c3ba18cf3d4e5eaecbc47a
Dylan Chong <dylan.chong@icloud.com>
2024-01-25 21:22:28 +0000

    Revert "Revert "Use format_on_save plugin""
    This reverts commit 574e87c896d44eeaad10ff3e9441ae8395388e9e.

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -30,8 +30,6 @@ lvim.builtin.which_key.vmappings['s'] = {
   }
 }
 
-lvim.format_on_save.enabled = true
-
 --------------- Custom plugins ---------------
 
 lvim.plugins = {
@@ -186,8 +184,22 @@ lvim.plugins = {
     end
   },
 
+  {
+    "elentok/format-on-save.nvim",
+    config = function()
+      local format_on_save = require("format-on-save")
+      local formatters = require("format-on-save.formatters")
+
+      format_on_save.setup({
+        formatter_by_ft = {
+          rust = formatters.lsp,
+          typescript = formatters.prettierd,
+        },
+      })
+    end
+  },
+
   -- TODO auto detect indent
-  -- TODO fix auto format does not respect prettier
   -- TODO param hints
   -- TODO search visual selection
 }
