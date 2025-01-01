fd055dcb7d7649eded6bbb4ac2401a2f975ec5f9
Dylan Chong <dylan.chong@icloud.com>
2025-01-01 16:36:02 +1300

    Remove nvim-httpyac

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -325,18 +325,6 @@ lvim.plugins = {
       })
     end,
   },
-
-  {
-    "abidibo/nvim-httpyac",
-    config = function()
-      require('nvim-httpyac').setup()
-      lvim.builtin.which_key.mappings['r']['h'] = {
-        ':NvimHttpYac<CR>',
-        "Run http request with Http Yac",
-      }
-    end
-  },
-
   -- TODO auto detect indent
 }
 
