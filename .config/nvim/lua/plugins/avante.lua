afcb9d56263823490c9b46f8e97acd8d78ecc6e9
Dylan Chong <dylan.chong@icloud.com>
2025-06-03 15:07:50 +1200

    avante upgrade

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -15,11 +15,15 @@ return {
       -- for example
       provider = "claude",
       auto_suggestions_provider = "claude",
-      claude = {
-        endpoint = "https://api.anthropic.com",
-        model = "claude-3-5-sonnet-20241022",
-        temperature = 0,
-        max_tokens = 4096,
+      providers = {
+        claude = {
+          endpoint = "https://api.anthropic.com",
+          model = "claude-3-5-sonnet-20241022",
+          extra_request = {
+            temperature = 0,
+            max_tokens = 4096,
+          }
+        },
       },
       -- openai = {
       --   endpoint = "https://api.openai.com/v1",
