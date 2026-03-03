e9a1e8723358ae9a7bf62036266771a820b8501d
Dylan Chong <dylan.chong@icloud.com>
2026-03-04 09:13:23 +1300

    MOD: config/keymaps.lua, plugins/tmux-navigation.lua

--- .config/nvim/lua/plugins/tmux-navigation.lua
+++ .config/nvim/lua/plugins/tmux-navigation.lua
@@ -1,5 +1,11 @@
 return {
   "alexghergh/nvim-tmux-navigation",
+  keys = {
+    { "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>" },
+    { "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>" },
+    { "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>" },
+    { "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>" },
+  },
   config = function()
     require("nvim-tmux-navigation").setup({})
   end,
