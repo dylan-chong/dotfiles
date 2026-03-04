7be5f9064ae30aaf7470110f23789d2ba2236a22
Dylan Chong <dylan.chong@icloud.com>
2026-03-04 16:51:22 +1300

    no float

--- .config/nvim/lua/plugins/claudecode.lua
+++ .config/nvim/lua/plugins/claudecode.lua
@@ -4,19 +4,11 @@ return {
     opts = {
       terminal = {
         snacks_win_opts = {
-          position = "float",
-          width = 0.9,
-          height = 0.9,
-          border = "rounded",
           keys = {
-            claude_hide = {
-              "<C-,>",
-              function(self)
-                self:hide()
-              end,
-              mode = "t",
-              desc = "Hide Claude",
-            },
+            nav_h = { "<C-h>", function() vim.cmd("stopinsert") vim.cmd("NvimTmuxNavigateLeft") end, mode = "t", desc = "Navigate left" },
+            nav_j = { "<C-j>", function() vim.cmd("stopinsert") vim.cmd("NvimTmuxNavigateDown") end, mode = "t", desc = "Navigate down" },
+            nav_k = { "<C-k>", function() vim.cmd("stopinsert") vim.cmd("NvimTmuxNavigateUp") end, mode = "t", desc = "Navigate up" },
+            nav_l = { "<C-l>", function() vim.cmd("stopinsert") vim.cmd("NvimTmuxNavigateRight") end, mode = "t", desc = "Navigate right" },
           },
         },
       },
